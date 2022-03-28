e26ae7aead578f310016ea699263632f02960fa0
Andrii Raikov <133789+raykov@users.noreply.github.com>
2022-03-28 11:29:24 +0200

    Update to Go 1.18

--- go.mod
+++ go.mod
@@ -1,10 +1,17 @@
 module github.com/raykov/mdtopdf
 
-go 1.16
+go 1.18
 
 require (
 	github.com/raykov/gofpdf v1.16.7
 	github.com/raykov/oksvg v0.0.2
-	github.com/srwiley/rasterx v0.0.0-20210519020934-456a8d69b780
-	github.com/yuin/goldmark v1.4.1
+	github.com/srwiley/rasterx v0.0.0-20220128185129-2efea2b9ea41
+	github.com/yuin/goldmark v1.4.11
+)
+
+require (
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/image v0.0.0-20211028202545-6944b10bf410 // indirect
+	golang.org/x/net v0.0.0-20210929193557-e81a3d93ecf6 // indirect
+	golang.org/x/text v0.3.6 // indirect
 )
--- renderer/writer.go
+++ renderer/writer.go
@@ -20,20 +20,20 @@ type Writer interface {
 
 // Styled describes what kind of behaviour we expect from styled elements
 type Styled interface {
-	NewSetFontStyleOption(string) func(style interface{}) error
-	NewAddFontStyleOption(string) func(style interface{}) error
-	NewFontFamilyOption(string) func(style interface{}) error
-	NewFontSizeOption(float64) func(style interface{}) error
+	NewSetFontStyleOption(string) func(style any) error
+	NewAddFontStyleOption(string) func(style any) error
+	NewFontFamilyOption(string) func(style any) error
+	NewFontSizeOption(float64) func(style any) error
 
-	NewTextColorOption(*color.Color) func(style interface{}) error
-	NewFillColorOption(*color.Color) func(style interface{}) error
+	NewTextColorOption(*color.Color) func(style any) error
+	NewFillColorOption(*color.Color) func(style any) error
 
-	NewAddMarginOption(float64) func(style interface{}) error
-	NewAddCellMarginOption(float64) func(style interface{}) error
-	NewSetCellMarginOption(float64) func(style interface{}) error
-	NewPaddingOption(float64) func(style interface{}) error
+	NewAddMarginOption(float64) func(style any) error
+	NewAddCellMarginOption(float64) func(style any) error
+	NewSetCellMarginOption(float64) func(style any) error
+	NewPaddingOption(float64) func(style any) error
 
-	WithStyle(...func(style interface{}) error)
+	WithStyle(...func(style any) error)
 	ResetStyle()
 }
 
